e58894e889fe3d9ea82b8cf5d727f4d5187af446
Ryan Bambrough <bambrough@macaroni-01.cs.wisc.edu>
2016-02-06 18:49:09 -0600

    repeate search save

--- list.c
+++ list.c
@@ -32,22 +32,77 @@ int main(int argc, char *argv[])
 {
     /* TODO: Complete the main method according to the steps outlined */
 
-    /* Open a file for reading */
+    FILE *input;
+    char buffer[100];
+    int inputs[MAX_INTS];
+    int length = 0;
+
+    if(argc != 2)
+    {
+        printf("The Input Arguments was bad");
+        exit(0);
+    }
+
+   /* Open a file for reading */
+
+    input = fopen(argv[1], "r");
+
+    if(input == NULL)
+    {
+        printf("Error in File Opening");
+        exit(0);
+    }
 
     /* Read the numbers from the file, into an array */
 
-    /* Print the array */
+    for(length; fgets(buffer, 100, input) != NULL; length++)
+    { 
+        inputs[length] = atoi(buffer);
+    }
+
+   /* Print the array */
+
+    print_array(inputs, length);
+    
 
     /* Create a linked list with the integers from the array */
+
+    struct node *head = create_list(inputs, length);
     
     /* Print the linked list */
 
+    print_list(head);
 
     /* Repeatedly prompt the user for a number to be searched.
     *  Search the array for the number and print out the result as shown in the specs.
     *  Search the linked list for the number and print out the result as shown in the specs.
     *  Stop prompting when the user enters 'q' (just the character q without the single quotes).
     */
+    char in[256];
+    while(1)
+    {
+        printf("\nEnter an element to be searched in the list and array: ");
+        scanf(" %s", &in);
+
+        if(in[0] == 'q')
+            break;
+
+        int index = search_array(inputs, length, atoi(in));
+
+        if(index == -1)
+        {
+            printf("\nElement %d not found in array.", atoi(in));
+
+        }
+        else
+        {
+            printf("\nElement %d found in the array at index: %d", atoi(in), index);
+
+        }
+    }
+
+printf("You say hello and I say goodbye");
+
 
     /* Create a sorted list(in ascending order) from the unsorted list */
 
@@ -72,13 +127,43 @@ int main(int argc, char *argv[])
 
 struct node* create_list(int intarray[], int len)
 {
-    /* TODO: Complete this function */ 
+    if(len == 0)
+    {
+        return NULL;
+    }
+
+    struct node *head;
+    head = (struct node*)malloc(sizeof(struct node));
+    (*head).data = intarray[0];
+
+    struct node *current = head;
+    (*current).next = NULL;
+
+    int i;
+    for( i = 1; i < len; i++)
+    {
+        struct node *next = (struct node*)malloc(sizeof(struct node));
+        (*next).data = intarray[i];
+        (*next).next = NULL;
+
+        (*current).next = next;
+
+        current = next;     
+    }
+
+    return head;
+
 }
 
 
 struct node* add_item_at_start(struct node *head, int data)
 {
-    /* TODO: Complete this function */
+    struct node* start = (struct node*)malloc(sizeof(struct node));
+    (*start).data = data;
+    
+    (*start).next = head;
+
+    return start;
 }
 
 int search_list(struct node *head, int element)
@@ -88,7 +173,19 @@ int search_list(struct node *head, int element)
 
 int search_array(int integers[], int numints, int element)
 {
-    /* TODO: Complete this function */
+
+    // Check what happens if there are more than on copy of the searched element
+
+    int i;
+    for(i = 0; i < numints; i++)
+    {
+        if(integers[i] == element)
+        {
+            return i;
+        }
+    }
+    
+    return -1;
 }
 
 int copy_list_to_array(struct node *head, int *array)
