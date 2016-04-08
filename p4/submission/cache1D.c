81ea7a41ec64dba3419c9f8826c3364fd2bc5057
Cheng-Hsiang Hsu <cheng-hsiang@galapagos-23.cs.wisc.edu>
2016-04-07 23:38:59 -0500

    add submission

---
+++ p4/submission/cache1D.c
@@ -0,0 +1,11 @@
+//Ryan Bambrough & Cheng-Hsiang Hsu
+//Section 1
+int arr[100000];
+
+int main ( int argc, const char* argv [] )
+{
+	for(int i = 0; i < 100000; i++)
+	{
+		arr[i] = i;
+	}
+}
---
+++ p4/submission/cache2Drows.c
@@ -0,0 +1,16 @@
+//Ryan Bambrough & Cheng-Hsiang Hsu
+//Section 1
+int arr[3000][500];
+
+int main (int argc, const char* argv[])
+{
+	int numrows = 3000;
+	int numcols = 500;
+		for(int i = 0; i < numrows; i++)
+		{
+			for(int j = 0; j < numcols; j++)
+			{
+				arr[i][j] = i + j;
+			}
+		}
+}
